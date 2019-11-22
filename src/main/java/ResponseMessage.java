4e0c7c5842489cc7245dd158ffcb9534a7b8355d
gleb <gleby-hleby@inbox.ru>
2019-11-23 00:55:31 +0300

    Scripted auto-commit on change (2019-11-23 00:55:31) by gitwatch.sh

--- src/main/java/ResponseMessage.java
+++ src/main/java/ResponseMessage.java
@@ -1,4 +1,14 @@
 public class ResponseMessage {
     public String packageId;
-    public
+    public boolean success;
+    public TestResult[] results;
+
+    public ResponseMessage(){
+
+    }
+
+    public ResponseMessage(String packageId, TestResult[] results){
+        this.packageId = packageId;
+        this.results = results;
+    }
 }
--- src/main/java/TestResult.java
+++ src/main/java/TestResult.java
@@ -1,7 +1,18 @@
 public class TestResult {
     public String packageId;
     public String result;
+    public String expected;
     public String testName;
     public Object[] params;
 
+    public TestResult() {
+    }
+
+    public TestResult(String packageId, String result,String expected, String testName, Object[] params) {
+        this.packageId = packageId;
+        this.result = result;
+        this.expected = expected;
+        this.testName = testName;
+        this.params = params;
+    }
 }
