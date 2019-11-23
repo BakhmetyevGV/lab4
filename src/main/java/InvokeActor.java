315bb986fa002ea3a377728441115c395c6982ac
gleb <gleby-hleby@inbox.ru>
2019-11-23 11:08:44 +0300

    Scripted auto-commit on change (2019-11-23 11:08:44) by gitwatch.sh

--- src/main/java/InvokeActor.java
+++ src/main/java/InvokeActor.java
@@ -1,11 +1,9 @@
 import akka.actor.AbstractActor;
-import akka.japi.pf.ReceiveBuilder;
 
 public class InvokeActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create()
-                .build();
+        return null;
     }
 }
