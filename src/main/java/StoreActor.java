c0b3740ac94ec39f9d8c2f6ebc042466cfe1bc76
gleb <gleby-hleby@inbox.ru>
2019-11-23 12:06:21 +0300

    Scripted auto-commit on change (2019-11-23 12:06:21) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -3,11 +3,14 @@ import akka.japi.pf.ReceiveBuilder;
 
 public class StoreActor extends AbstractActor {
 
+    public Map<>
+
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestResult.class, msg ->{
-                    
+
                 });
     }
 }
