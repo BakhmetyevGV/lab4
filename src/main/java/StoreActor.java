2749a5e78ce141fd2fb3111249787a27b7f78b29
gleb <gleby-hleby@inbox.ru>
2019-11-23 12:14:21 +0300

    Scripted auto-commit on change (2019-11-23 12:14:21) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -18,7 +18,7 @@ public class StoreActor extends AbstractActor {
                     if(storage.get(msg.packageId) == null){
                         storage.put(msg.packageId, new ArrayList<>());
                     }
-                    storage.get
+                    storage.get(msg.packageId).add(msg.)
 
                 });
     }
