5af858c98eac19aae7584e9ccbd4d4ad46542c58
gleb <gleby-hleby@inbox.ru>
2019-11-23 13:07:58 +0300

    Scripted auto-commit on change (2019-11-23 13:07:58) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,4 +3,13 @@ public class Test {
     public String expectedResult;
     public Object[] params;
 
+    public Test(){
+
+    }
+
+    public Test(String testName, String expectedResult, Object[] params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 }
